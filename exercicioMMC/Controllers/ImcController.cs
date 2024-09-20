c1e8fc3965a85f36ba786c9a7b334d39f38a5290
gustavolivera <social.gustavosilva@gmail.com>
2024-09-20 10:18:57 -0300

    Add project files.

---
+++ exercicioMMC/Controllers/ImcController.cs
@@ -0,0 +1,20 @@
+﻿using exercicioMMC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace exercicioMMC.Controllers
+{
+	public class ImcController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Calcular(ImcModel valores) {
+			ModelState.Clear();
+			valores.Calcular();
+			return View("Index", valores);
+		}
+	}
+}
---
+++ exercicioMMC/Models/ImcModel.cs
@@ -0,0 +1,34 @@
+﻿namespace exercicioMMC.Models
+{
+	public class ImcModel
+	{
+        public double? Altura { get; set; }
+        public double? Peso { get; set; }
+        public double? Valor { get; set; }
+        public string? Situacao { get; set; }
+
+		public void Calcular()
+		{
+			Valor = Peso / Math.Pow(Altura.Value, 2);
+			
+			switch (Valor)
+			{
+				case < 18.5: Situacao = "Abaixo do peso";
+					break;
+				case < 25: Situacao = "Peso normal";
+					break;
+				case < 30: Situacao = "Sobrepeso";
+					break;
+				default: Situacao = "Obesidade";
+					break;
+			}
+			
+
+		}
+
+		
+
+	}
+
+   
+}
